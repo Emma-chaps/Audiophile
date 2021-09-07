c2167d371a7e51f25e99da67fee24dcbb9dbd042
Emma <em.charpin@gmail.com>
2021-09-07 17:56:21 +0200

    initialisation project + add router

---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./../pages/Home";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className='App'>
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
